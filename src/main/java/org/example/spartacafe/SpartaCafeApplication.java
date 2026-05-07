8c6fa046d5bfc0b690d2ccdd74622d5b7dada48c
hyeonmin02 <hyenmin5@gmail.com>
2026-05-07 22:39:34 +0900

    Initial commit

---
+++ src/main/java/org/example/spartacafe/SpartaCafeApplication.java
@@ -0,0 +1,13 @@
+package org.example.spartacafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaCafeApplication.class, args);
+    }
+
+}
